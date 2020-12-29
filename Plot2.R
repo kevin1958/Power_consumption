13abb72bf9533fe8f8c87d6a5e824fbf20b480f1
kevin1958 <k.nellimela@gmail.com>
2020-12-29 14:01:53 +0530

    modified

---
+++ Plot2.R
@@ -0,0 +1,14 @@
+#Set working directory
+setwd("C:/Users/iperlino/Documents/R/week3assignment/ExData_Plotting1")
+
+#Reading, naming and subsetting power consumption data
+dataFile <- "./household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str(subSetData)
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+png("plot2.png", width=480, height=480)
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
